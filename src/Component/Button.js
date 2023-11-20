8fdf7965692cee1e43076461e3eb0f4fc585fbc4
Sumaiya Shah <sumaiyashaha27@gmail.com>
2023-11-20 16:05:55 +0530

    first commit

---
+++ src/Component/Button.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Button = ({ onClickHandler, value, title }) => {
+  return (
+    <div>
+      <button onClick={onClickHandler} value={value} className="btns">
+      {title}
+    </button>
+    </div>
+  )
+}
+export default Button;
